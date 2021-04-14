77fbee41be3c9e2b6c44b6d2d94724f51f125368
warinthon30 <jaitrong3016@gmail.com>
2021-04-15 00:28:08 +0700

    commit1

---
+++ frontend/pages/index.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import Layout from "../components/layout";
+import Navbar from "../components/navbar";
+import styles from "../styles/Home.module.css";
+
+export default function Home({ token }) {
+  return (
+    <Layout>
+      <Head>
+        <title>First Page</title>
+      </Head>
+      <div className={styles.container}>
+        <Navbar />
+        <h1>Home page</h1>
+      <p>Welcome</p> 
+      </div>
+    </Layout>
+  );
+}
+
+export function getServerSideProps({ req, res }) {
+  // console.log("token from cookie: ",cookie.get("token"))
+  // console.log('req: ', req.headers)
+  return { props: { token: req.cookies.token || "" } };
+}
